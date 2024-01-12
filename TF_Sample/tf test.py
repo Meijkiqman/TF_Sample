7696057387d637db8b59d9dbdc0bab5e4ca0351a
Meijkiqman <sivertkindberg@hotmail.com>
2024-01-12 22:08:43 +0100

    runs

--- TF_Sample/tf test.py
+++ TF_Sample/tf test.py
@@ -3,7 +3,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import tensorflow as tf 
 from tensorflow import keras
-from keras import layers
+from keras import layers, regularizers
 from keras.datasets import cifar10
 
 print(tf.config.list_physical_devices('CPU'))
@@ -33,18 +33,26 @@ print(model.summary())
 
 def my_Model():
     inputs = keras.Input(shape=(32, 32, 3))
-    x = layers.Conv2D(32, 3)(inputs)
+    x = layers.Conv2D(
+        32, 3, padding='same', kernel_regularizer=regularizers.l2(0.01),
+        )(inputs)
     x = layers.BatchNormalization()(x)
     x = keras.activations.relu(x)
-    x = layers.MaxPooling2D()
-    x = layers.Conv2D(64, 5, paddings='same')(x)
+    x = layers.MaxPooling2D()(x)
+    x = layers.Conv2D(
+        64, 3, padding='same', kernel_regularizer=regularizers.l2(0.01),
+    )(x)
     x = layers.BatchNormalization()(x)
     x = keras.activations.relu(x)
-    x = layers.Conv2D(128, 3)(x)
+    x = layers.Conv2D(
+        128, 3, padding='same', kernel_regularizer=regularizers.l2(0.01),
+    )(x)
     x = layers.BatchNormalization()(x)
     x = keras.activations.relu(x)
     x = layers.Flatten()(x)
-    x = layers.Dense(64, activation='relu')(x)
+    x = layers.Dense(64, activation='relu', kernel_regularizer=regularizers.l2(0.01),
+    )(x)
+    x = layers.Dropout(0.5)(x)
     outputs = layers.Dense(10)(x)
     model = keras.Model(inputs=inputs, outputs=outputs)
     return model
@@ -57,7 +65,7 @@ model.compile(
     metrics=["accuracy"],
 )
 
-model.fit(x_train, y_train, batch_size=64, epochs=10, verbose=2)
+model.fit(x_train, y_train, batch_size=64, epochs=150, verbose=2)
 
 model.evaluate(x_test, y_test, batch_size=64, verbose=2)
 
